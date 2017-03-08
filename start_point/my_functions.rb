d70df96a7af79bcad47d7a68d4b0087032fd5f74
hatwell <caroline.hatwell@gmail.com>
2017-03-08 16:28:23 +0000

    Written functions that passed tests.

--- start_point/my_functions.rb
+++ start_point/my_functions.rb
@@ -1,1 +1,16 @@
-#comment
+def add_array_lengths(array1, array2)
+  return array1.length + array2.length
+end
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def find_item(array, value)
+  array.index(value) ? true : false
+end
+
+def get_first_key(teachers_wallets)
+  teachers_wallets.first.first
+
+end
